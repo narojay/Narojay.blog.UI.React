04e9d9ab6c71b3744cef1d49e51f54845ab14274
narojay <hj200812@126.com>
2022-07-28 09:39:38 +0800

    feat #顶部进度条

--- src/utils/http.jsx
+++ src/utils/http.jsx
@@ -1,26 +1,29 @@
 import axios from "axios"
-import { message, Spin } from "antd"
-import ReactDOM from "react-dom"
+import { message } from "antd"
+// import ReactDOM from "react-dom"
+import nProgress from "nprogress"
+import "nprogress/nprogress.css"
+import "./index.css"
 message.config({ maxCount: 3 })
-let requestCount = 0
+// let requestCount = 0
 
-// 显示loading
-function showLoading() {
-  if (requestCount === 0) {
-    var dom = document.createElement("div")
-    dom.setAttribute("id", "loading")
-    document.body.appendChild(dom)
-    ReactDOM.render(<Spin tip="加载中..." size="large" />, dom)
-  }
-  requestCount++
-}
-// 隐藏loading
-function hideLoading() {
-  requestCount--
-  if (requestCount === 0) {
-    document.body.removeChild(document.getElementById("loading"))
-  }
-}
+// // 显示loading
+// function showLoading() {
+//   if (requestCount === 0) {
+//     var dom = document.createElement("div")
+//     dom.setAttribute("id", "loading")
+//     document.body.appendChild(dom)
+//     ReactDOM.render(<Spin tip="加载中..." size="large" />, dom)
+//   }
+//   requestCount++
+// }
+// // 隐藏loading
+// function hideLoading() {
+//   requestCount--
+//   if (requestCount === 0) {
+//     document.body.removeChild(document.getElementById("loading"))
+//   }
+// }
 // const MOCK_API =
 //   "https://www.fastmock.site/mock/ea8ceeffbca9c66d695f3bc1fead877b/backstage/"
 
@@ -30,14 +33,14 @@ const http = axios.create({
   baseURL: process.env.REACT_APP_API,
   // baseURL: process.env.REACT_APP_ENV === 'mock' ? MOCK_API : window.location.origin,
   // withCredentials: true,
-  headers: { isLoading: false },
+  headers: { isLoading: true },
 
   timeout: 1000 * 60 * 3
 })
 http.interceptors.request.use(
   function (config) {
     if (config.headers.isLoading !== false) {
-      showLoading()
+      nProgress.start()
     }
     if (localStorage.getItem("jwt")) {
       http.defaults.headers.common["Authorization"] =
@@ -64,7 +67,7 @@ http.interceptors.request.use(
 http.interceptors.response.use(
   function (response) {
     if (response.config.headers.isLoading !== false) {
-      hideLoading()
+      nProgress.done()
     }
     if (response.status === 200 || response.status === 204) {
       return response.data
@@ -74,6 +77,9 @@ http.interceptors.response.use(
     }
   },
   function (error) {
+    if (error.headers.isLoading !== false) {
+      nProgress.done()
+    }
     if (error.code === 4002) {
       message.error(error.message)
       window.location.href = "#/login"
